5356b4f5c5034109783687fc6e5b49b7846aa7bc
John Stamp <john.stamp@directski.com>
2014-06-15 03:15:14 +0100

    fixed up node app

--- app.js
+++ app.js
@@ -13,14 +13,34 @@ var yelp = require("yelp").createClient({
 
 
 
-app.get('/yelp/search/:term/city/:city', function(req, res) {
+app.get('/yelp/search/:term/location/:location', function(req, res) {
 
 	var term=req.params.term;
-	var city=req.params.city;
+	var location=req.params.location;
 
-	yelp.search({term: term, location: city}, function(error, data) {
+	yelp.search({term: term, location: location}, function(error, data) {
 		console.log(req.params.term);
-		console.log(req.params.city);
+		console.log(req.params.location);
+		res.header("Access-Control-Allow-Origin", "*")
+  		//console.log(error);
+  		//console.log(data);
+  		res.setHeader('Content-Type', 'application/json');
+		res.send(data);
+		});
+
+
+  //res.type('text/plain'); // set content-type
+  //res.send('i am a beautiful butterfly'+results); // send text response
+});
+app.get('/yelp/search/category/:category_filter/location/:location', function(req, res) {
+
+	var category=req.params.category_filter;
+	var location=req.params.location;
+
+	yelp.search({category: category, location: location}, function(error, data) {
+		console.log(category);
+		console.log(location);
+		res.header("Access-Control-Allow-Origin", "*")
   		//console.log(error);
   		//console.log(data);
   		res.setHeader('Content-Type', 'application/json');
@@ -32,4 +52,22 @@ app.get('/yelp/search/:term/city/:city', function(req, res) {
   //res.send('i am a beautiful butterfly'+results); // send text response
 });
 
-app.listen(process.env.PORT || 4730);
+
+app.get('/yelp/search/:term/longtitude/:long/latitude/:lat', function(req, res) {
+
+	var term=req.params.term;
+	var longtitude=req.params.long;
+	var latitude=req.params.lat;
+
+		yelp.search({term: term, longtitude:longtitude,latitude:latitude}, function(error, data) {
+		console.log(req.params.term);
+		console.log(data);
+		res.header("Access-Control-Allow-Origin", "*")
+  		//console.log(error);
+  		//console.log(data);
+  		res.setHeader('Content-Type', 'application/json');
+		res.send(data);
+		});
+
+});
+app.listen(process.env.PORT || 8080);
