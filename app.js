1e5e50bb77dc30155943d8df74c4d1fb518862a2
John Stamp <john.stamp@directski.com>
2014-07-30 17:56:14 +0100

    removed secret keys for security

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var app = express();
 
 
 var yelp = require("yelp").createClient({
-	  consumer_key: "oX2sb_xE-GeTSb-_bdRvNQ", 
-	  consumer_secret: "R0f-oxU75q47LGK05beLY6uOJDQ",
-	  token: "dM_EefXlYbxguf6hWzIyq6LovaMeUuFa",
-	  token_secret: "kUSpqYdyG63fsowdaBrQLZSzGsA"
+	  consumer_key: "***********************", 
+	  consumer_secret: "***********************",
+	  token: "***********************",
+	  token_secret: "***********************"
 		});
 
 
@@ -29,8 +29,6 @@ app.get('/yelp/search/:term/location/:location', function(req, res) {
 		});
 
 
-  //res.type('text/plain'); // set content-type
-  //res.send('i am a beautiful butterfly'+results); // send text response
 });
 app.get('/yelp/search/category/:category_filter/location/:location', function(req, res) {
 
@@ -41,15 +39,10 @@ app.get('/yelp/search/category/:category_filter/location/:location', function(re
 		console.log(category);
 		console.log(location);
 		res.header("Access-Control-Allow-Origin", "*")
-  		//console.log(error);
-  		//console.log(data);
   		res.setHeader('Content-Type', 'application/json');
 		res.send(data);
 		});
 
-
-  //res.type('text/plain'); // set content-type
-  //res.send('i am a beautiful butterfly'+results); // send text response
 });
 
 
@@ -62,21 +55,20 @@ app.get('/yelp/search/:term/longtitude/:longtitude/latitude/:latitude', function
 	console.log('ing from long and lat');
 	console.log(longlat);
 
-		yelp.search({term: term,ll:longlat,limit:5}, function(error, data) {
-
+	yelp.search({term: term,ll:longlat,limit:5}, function(error, data) 
+	{
 		res.header("Access-Control-Allow-Origin", "*")
-  		//console.log(error);
   		console.log(data);
   		res.setHeader('Content-Type', 'application/json');
 		res.send(data);
-		});
+	});
 
 });
 
 app.get('*', function(req, res){
 	console.log("404 hit");
 	res.header("Access-Control-Allow-Origin", "*")
-  res.send('what???', 404);
+  	res.send('what???', 404);
 });
 
 var port = Number(process.env.PORT || 8080);
